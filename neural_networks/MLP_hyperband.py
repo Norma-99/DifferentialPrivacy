15f7a2913926d5b884ab6afe2a7bb6beffaf686a
musergi <sergi.mus@gmail.com>
2020-07-28 17:03:25 +0200

    Updated MLP hyperband

--- neural_networks/MLP_hyperband.py
+++ neural_networks/MLP_hyperband.py
@@ -1,4 +1,5 @@
 #This will import ADULT dataset and use Hyperband to decide which model is the most optimal for the dataset
+import argparse
 from functools import partial
 
 import pickle
@@ -7,46 +8,50 @@ from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.optimizers import Adam
 import kerastuner as kt
 
-EPOCHS = 5 
-HIDDEN = 3
-
 def load_data(path):
     with open(path, 'rb') as f:
         return pickle.load(f)
 
-def model_builder(input_shape, hp):
+def model_builder(input_shape, config, hp):
     model = tf.keras.models.Sequential()
     model.add(Flatten(input_shape=input_shape))
 
-    for i in range(HIDDEN):
-        hp_filters = hp.Int(f'filters{i}', min_value=128, max_value=2056, step=8)
+    for i in range(config['hidden_layers']):
+        hp_filters = hp.Int(
+            f'filters{i}',
+            min_value=config['filters_min_value'],
+            max_value=config['filters_max_value'],
+            step=config['filters_step'])
         model.add(Dense(units = hp_filters, activation = 'relu'))
     model.add(Dense(1, activation='sigmoid'))
 
-    hp_learning_rate = hp.Choice('learning_rate', values = [1e-2, 1e-3, 1e-4])
+    hp_learning_rate = hp.Choice('learning_rate', values = config['learning_rates'])
     model.compile(optimizer = Adam(learning_rate = hp_learning_rate),
                 loss = 'binary_crossentropy', 
                 metrics = ['accuracy'])
     return model
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', required=True)
+    args = parser.parse_args()
 
     # Load data
     print("Loading data")
-    x_test, y_test = load_data('datasets/extended/validation/val_dataset.pickle')
-    x_train, y_train = load_data('datasets/extended/test/split1/test_dataset.pickle')
+    x_train, y_train = load_data(args.config['train_dataset'])
+    x_test, y_test = load_data(args.config['val_dataset'])
 
     tuner = kt.Hyperband(
-    partial(model_builder, (x_train.shape[1:])),
+    partial(model_builder, x_train.shape[1:], args.config),
     objective = 'val_accuracy',
-    max_epochs = EPOCHS,
+    max_epochs = args.config['epochs'],
     directory = 'result_dir',
-    project_name = 'test2_from128_to2056_in8')
+    project_name = args.config['project_name'])
 
     tuner.search(
         x_train,
         y_train,
-        epochs = EPOCHS,
+        epochs = args.config['epochs'],
         validation_data = (x_test, y_test))
 
     models = tuner.get_best_models(num_models=5)
