32b2004f43ea8fe219eac1ae7b004db36b5f4109
Norma-99 <normaescg@gmail.com>
2020-09-15 20:53:17 +0200

    fixed gradient_operations

--- differential_privacy/gradient_operations.py
+++ differential_privacy/gradient_operations.py
@@ -4,8 +4,10 @@ def gradient_calc(initial, final):
         iteration_deltas.append(final_layer_weights - inital_layer_weights)
     return iteration_deltas
 
+#mirar
 def gradient_apply(weights, gradient):
-    pass
-
-
-    
+    new_weights = []
+    for layer_weights, layer_gradient in zip(weights, gradient):
+            new_weights.append(layer_weights + layer_gradient)
+    return new_weights
+ 
