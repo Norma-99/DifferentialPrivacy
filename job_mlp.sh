a472db04c5eab50a8424c33bd35b0a0481173972
Norma-99 <normaescg@gmail.com>
2020-10-17 20:01:03 +0200

    cleaned code

--- job_mlp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH --chdir=/scratch/nas/4/norma/DifferentialPrivacy
-#SBATCH --output=/scratch/nas/4/norma/.log/stdout-%j.out
-#SBATCH --error=/scratch/nas/4/norma/.log/stderr-%j.out
-PYTHON="/scratch/nas/4/norma/venv/bin/python"
-
-$PYTHON neural_networks/MLP_hyperband.py
