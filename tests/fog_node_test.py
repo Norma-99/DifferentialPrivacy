6a3cd459208ef67be5754febb70fbd47c9a7e87e
musergi <sergi.mus@gmail.com>
2020-10-25 17:52:03 +0100

    Added testing methods, implementation to be written

--- tests/fog_node_test.py
+++ tests/fog_node_test.py
@@ -1,16 +1,31 @@
 import unittest
-from differential_privacy.model.fog_node import FogNode
-from differential_privacy.model.server import Server
+import numpy as np
+from differential_privacy.model import FogNode
+
+
+class StubGradientFolder:
+    def __init__(self):
+        self.gradients = None
+
+    def fold(gradients):
+        self.gradients = gradients
+
+
+class StubModel:
+    def fit(dataset): 
+        return [np.ones(3, 3), np.ones(3, 1)]
+
 
 class FogNodeTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.fog_node = FogNode()
+    def setUp(self):
+        self.gradient_folder = StubGradientFolder()
+        self.fog_node = FogNode(3, self.gradient_folder)
 
-    def test_server_setter(self):
-        server = Server(None)
-        self.fog_node.set_server(server)
-        self.assertEqual(self.fog_node.server, server)
+    def test_model_request_on_dataset_sending(self):
+        pass
 
+    def test_gradient_fold_on_iteration_end(self):
+        pass
 
 
 if __name__ == '__main__':
