f28316861b47d4fa935917d809488dacc9b4d0d9
Gutierrez Escobar <norma@sert-entry-2.ac.upc.es>
2020-12-30 11:28:03 +0100

    results

--- job.sh
+++ job.sh
@@ -7,7 +7,11 @@
 PYTHON="/scratch/nas/4/norma/venv/bin/python"
 CONFIG_FOLDER="/scratch/nas/4/norma/DifferentialPrivacy/configs" 
 
-for i in 3 5 7
+for i in 1
 do
-	$PYTHON -m differential_privacy --config=$CONFIG_FOLDER/hyper/${i}nodes.json
+	for j in {1..50}
+	do
+		$PYTHON -m differential_privacy --config=$CONFIG_FOLDER/adult/${i}nodes.json
+		mv ./results/adult/${i}nodes/adult_${i}nodes.csv ./results/adult/${i}nodes/adult_${i}nodes_it${j}.csv
+	done
 done
